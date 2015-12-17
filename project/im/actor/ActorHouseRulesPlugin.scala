ba89b9043c81009c427c443ebcbed71fb5c3d873
prettynatty <fear@loathing.in>
2015-12-17 03:25:44 +0300

    fix: fixed imports

--- project/im/actor/ActorHouseRulesPlugin.scala
+++ project/im/actor/ActorHouseRulesPlugin.scala
@@ -5,40 +5,38 @@ import Keys._
 import bintray.Keys._
 
 object ActorHouseRulesPlugin extends AutoPlugin {
-  object autoImport {
-    lazy val actorPublishSettings = publishSettings("im.actor")
+  lazy val actorPublishSettings = publishSettings("im.actor")
 
-    lazy val actorServerPublishSettings = publishSettings("im.actor.server")
+  lazy val actorServerPublishSettings = publishSettings("im.actor.server")
 
-    def actorDefaultSettings(organization: String) = publishSettings(organization) ++ actorCompileSettings
+  def actorDefaultSettings(organization: String = "im.actor") = publishSettings(organization) ++ actorCompileSettings
 
-    private def publishSettings(organization: String) = Seq(
-      organization := "im.actor",
-      bintrayOrganization in bintray := Some("actor"),
-      repository in bintray := "maven",
-      licenses := Seq("Apache v2" -> url("https://github.com/actorapp/actor-platform/blob/master/LICENSE"))
-    )
+  private def publishSettings(organization: String) = Seq(
+    organization := "im.actor",
+    bintrayOrganization in bintray := Some("actor"),
+    repository in bintray := "maven",
+    licenses := Seq("Apache v2" -> url("https://github.com/actorapp/actor-platform/blob/master/LICENSE"))
+  )
 
-    private lazy val actorCompileSettings = Seq(
-      scalacOptions in Compile ++= Seq(
-        "-target:jvm-1.8",
-        "-Ybackend:GenBCode",
-        "-Ydelambdafy:method",
-        "-Yopt:l:classpath",
-        "-encoding", "UTF-8",
-        "-deprecation",
-        "-unchecked",
-        "-feature",
-        "-language:higherKinds",
-        "-Xfatal-warnings",
-        "-Xlint",
-        "-Xfuture",
-        "-Ywarn-dead-code",
-        "-Ywarn-infer-any",
-        "-Ywarn-numeric-widen"
-      ),
-      javaOptions ++= Seq("-Dfile.encoding=UTF-8"),
-      javacOptions ++= Seq("-source", "1.8", "-target", "1.8", "-Xlint:unchecked", "-Xlint:deprecation"),
-    )
-  }
+  private lazy val actorCompileSettings = Seq(
+    scalacOptions in Compile ++= Seq(
+      "-target:jvm-1.8",
+      "-Ybackend:GenBCode",
+      "-Ydelambdafy:method",
+      "-Yopt:l:classpath",
+      "-encoding", "UTF-8",
+      "-deprecation",
+      "-unchecked",
+      "-feature",
+      "-language:higherKinds",
+      "-Xfatal-warnings",
+      "-Xlint",
+      "-Xfuture",
+      "-Ywarn-dead-code",
+      "-Ywarn-infer-any",
+      "-Ywarn-numeric-widen"
+    ),
+    javaOptions ++= Seq("-Dfile.encoding=UTF-8"),
+    javacOptions ++= Seq("-source", "1.8", "-target", "1.8", "-Xlint:unchecked", "-Xlint:deprecation"),
+  )
 }
