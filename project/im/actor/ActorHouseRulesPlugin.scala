78babdaccc08a393768498304eaed9630314b84a
prettynatty <fear@loathing.in>
2015-12-17 03:17:54 +0300

    feat: compiler settings

--- project/im/actor/ActorHouseRulesPlugin.scala
+++ project/im/actor/ActorHouseRulesPlugin.scala
@@ -10,11 +10,35 @@ object ActorHouseRulesPlugin extends AutoPlugin {
 
     lazy val actorServerPublishSettings = publishSettings("im.actor.server")
 
+    def actorDefaultSettings(organization: String) = publishSettings(organization) ++ actorCompileSettings
+
     private def publishSettings(organization: String) = Seq(
       organization := "im.actor",
       bintrayOrganization in bintray := Some("actor"),
       repository in bintray := "maven",
       licenses := Seq("Apache v2" -> url("https://github.com/actorapp/actor-platform/blob/master/LICENSE"))
+    )
+
+    private lazy val actorCompileSettings = Seq(
+      scalacOptions in Compile ++= Seq(
+        "-target:jvm-1.8",
+        "-Ybackend:GenBCode",
+        "-Ydelambdafy:method",
+        "-Yopt:l:classpath",
+        "-encoding", "UTF-8",
+        "-deprecation",
+        "-unchecked",
+        "-feature",
+        "-language:higherKinds",
+        "-Xfatal-warnings",
+        "-Xlint",
+        "-Xfuture",
+        "-Ywarn-dead-code",
+        "-Ywarn-infer-any",
+        "-Ywarn-numeric-widen"
+      ),
+      javaOptions ++= Seq("-Dfile.encoding=UTF-8"),
+      javacOptions ++= Seq("-source", "1.8", "-target", "1.8", "-Xlint:unchecked", "-Xlint:deprecation"),
     )
   }
 }
